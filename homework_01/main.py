9929caafb4bc6ef713d08164e07a66311dd73674
Oleg <v.oleg159@gmail.com>
2024-04-22 20:40:05 +0400

    update names

--- homework_01/main.py
+++ homework_01/main.py
@@ -29,6 +29,8 @@ def filter_numbers(nums, filter_numbers_):
         return result
     elif filter_numbers_ == PRIME:
         def is_prime(n):
+            if n < 2:
+                return False
             for i in range(2, int(n ** 0.5) + 1):
                 if n % i == 0:
                     return False
@@ -41,4 +43,4 @@ def filter_numbers(nums, filter_numbers_):
         print("Invalid filter type")
 
 
-filter_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], PRIME)
+filter_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], PRIME)
