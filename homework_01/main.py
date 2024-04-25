843c183ffe4c300836330d7e9c5d130d374c036a
Oleg <v.oleg159@gmail.com>
2024-04-25 11:13:18 +0400

    move "def is_prime" to the main line

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,46 +1,26 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-
-
 def power_numbers(*nums):
     sq = [num ** 2 for num in nums]
-    # print(sq)
     return sq
 
-
-power_numbers(3, 5, 6, 7)
-
-# filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 def filter_numbers(nums, filter_numbers_):
     if filter_numbers_ == ODD:
         result = [num for num in nums if num % 2 != 0]
-        # print(result)
         return result
     elif filter_numbers_ == EVEN:
         result = [num for num in nums if num % 2 == 0]
-        print(result)
         return result
     elif filter_numbers_ == PRIME:
-        def is_prime(n):
-            if n < 2:
-                return False
-            for i in range(2, int(n ** 0.5) + 1):
-                if n % i == 0:
-                    return False
-            return True
-
         result = [num for num in nums if is_prime(num)]
-        # print(result)
         return result
-    else:
-        print("Invalid filter type")
-
-
-filter_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], PRIME)
